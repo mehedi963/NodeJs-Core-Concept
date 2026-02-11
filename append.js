36b87c424a3ebdde3ef042938a93b25a51467e6c
Mehedi <mh0626254@gmail.com>
2026-02-12 03:08:20 +0600

    Append File Done

---
+++ append.js
@@ -0,0 +1,12 @@
+const fs = require("fs")
+
+// fs.writeFileSync("./output/app.log", "Application Started\n");
+// console.log("File Created");
+
+const logEntry1 = `\n ${new Date().toISOString()} user logged in \n`;
+fs.appendFileSync("./output/app.log", logEntry1);
+console.log("File appeneded successfully")
+
+const logEntry2 = `${new Date().toISOString()} Data fetched \n`;
+fs.appendFileSync("./output/app.log", logEntry2);
+console.log("Task complete");
